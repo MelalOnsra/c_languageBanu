5557a3440f9c6ce59501fffb1f1f14602cde266b
Berkant <119416073+MelalOnsra@users.noreply.github.com>
2022-11-29 14:57:16 +0300

    Update username_pass_func.c

--- username_pass_func.c
+++ username_pass_func.c
@@ -1,23 +1,32 @@
 //create a username and password and check it with function
 
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
-#include "ctype.h" // for isdigit()
+#include <stdio.h>
+#include <string.h>
+#define user "melal"
+#define pass "afyon03"        
+int main()
+{
+    char username[20], password[20];
 
-void login(char username[], char password[]) {
-//   your code here  
-//   code burada
-  
-}
+    //-----LOGIN PHASE-----
+    printf("Please Enter username:");
+    scanf("%s", &username);
 
-int main() {
-    char username[20];
-    char password[20];
-    printf("Enter your username: ");
-    scanf("%s", username);
-    printf("Enter your password: ");
-    scanf("%s", password);
-    login(username, password);
-    return 0;
+    printf("\nPlease enter password:");
+    scanf("%s", &password);
+    do
+    {
+
+        printf("Wrong username or password try again!\n");
+
+        printf("\nPlease Enter username:");
+        scanf("%s", &username);
+
+        printf("\nPlease enter password:");
+        scanf("%s", &password);
+
+    }
+    while (strcmp(username, user) != 0 && strcmp(password, pass) != 0);
+
+    printf("Correct username and password.\n\nWelcome melal!\n\nWhat would you like to do?");
 }
